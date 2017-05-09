71726396505b7720381af61f6882188d2c65737e
ThirteenAG <thirteenag@gmail.com>
2017-05-09 20:04:59 +0300

    :octocat:

---
+++ screens/convert.bat
@@ -0,0 +1,7 @@
+@echo off
+
+FOR /R %%a IN (*.png) DO ( 
+   (Echo "%%a" | FIND /I "logo.png" 1>NUL) || (
+       convert "%%~a" -strip -resize x367 -background white -gravity center -extent 652x367 "%%~dpna.jpg"
+   )
+)
