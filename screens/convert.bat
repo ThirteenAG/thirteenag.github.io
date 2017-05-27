e5ebfb54a32b3f340888a49b05d5341a1e9001c8
ThirteenAG <thirteenag@gmail.com>
2017-05-27 17:01:27 +0300

    -

--- screens/convert.bat
+++ screens/convert.bat
@@ -2,6 +2,6 @@
 
 FOR /R %%a IN (*.png) DO ( 
    (Echo "%%a" | FIND /I "logo.png" 1>NUL) || (
-       convert "%%~a" -strip -resize x367 -background white -gravity center -extent 652x367 "%%~dpna.jpg"
+       convert.ex "%%~a" -strip -resize x367 -background white -gravity center -extent 652x367 "%%~dpna.jpg"
    )
 )
