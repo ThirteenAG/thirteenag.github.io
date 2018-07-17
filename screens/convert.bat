fe121972e0d3f63fb6b8753c7247ab22e4704da7
ThirteenAG <thirteenag@gmail.com>
2018-07-17 15:07:15 +0300

    screens layout update

--- screens/convert.bat
+++ screens/convert.bat
@@ -5,3 +5,9 @@ FOR /R %%a IN (*.png) DO (
        convert.ex "%%~a" -strip -resize x367 -background white -gravity center -extent 652x367 "%%~dpna.jpg"
    )
 )
+
+convert.ex "./driverpl/main1.png" -strip -resize 652x -background white -gravity center -extent 652x "./driverpl/main1.jpg"
+convert.ex "./driverpl/main2.png" -strip -resize 652x -background white -gravity center -extent 652x "./driverpl/main2.jpg"
+
+convert.ex "./flatoutuc/main1.png" -strip -resize 652x -background white -gravity center -extent 652x "./flatoutuc/main1.jpg"
+convert.ex "./flatoutuc/main2.png" -strip -resize 652x -background white -gravity center -extent 652x "./flatoutuc/main2.jpg"
