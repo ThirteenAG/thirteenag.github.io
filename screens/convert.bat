1adf7b0e892c99c306f1aeab628bdf86bf3d4a8b
ThirteenAG <thirteenag@gmail.com>
2018-09-06 15:18:18 +0300

    -

--- screens/convert.bat
+++ screens/convert.bat
@@ -11,3 +11,6 @@ convert.ex "./driverpl/main2.png" -strip -resize 652x -background white -gravity
 
 convert.ex "./flatoutuc/main1.png" -strip -resize 652x -background white -gravity center -extent 652x "./flatoutuc/main1.jpg"
 convert.ex "./flatoutuc/main2.png" -strip -resize 652x -background white -gravity center -extent 652x "./flatoutuc/main2.jpg"
+
+convert.ex "./bully/main1.png" -strip -resize 652x -background white -gravity center -extent 652x "./bully/main1.jpg"
+convert.ex "./bully/main2.png" -strip -resize 652x -background white -gravity center -extent 652x "./bully/main2.jpg"
