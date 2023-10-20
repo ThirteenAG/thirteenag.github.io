f29c8df7aa769ad3970bf051fdc5e896d98a743b
ThirteenAG <thirteenag@gmail.com>
2023-10-20 17:22:55 +0800

    -

--- screens/convert.bat
+++ screens/convert.bat
@@ -18,5 +18,11 @@ convert.ex "./bully/main2.png" -strip -resize 652x -background white -gravity ce
 convert.ex "./condemned/main1.png" -strip -resize 652x -background white -gravity center -extent 652x "./condemned/main1.jpg"
 convert.ex "./condemned/main2.png" -strip -resize 652x -background white -gravity center -extent 652x "./condemned/main2.jpg"
 
+convert.ex "./re4/main1.png" -strip -resize 652x -background white -gravity center -extent 652x "./re4/main1.jpg"
+convert.ex "./re4/main2.png" -strip -resize 652x -background white -gravity center -extent 652x "./re4/main2.jpg"
+
+convert.ex "./re5/main1.png" -strip -resize 652x -background white -gravity center -extent 652x "./re5/main1.jpg"
+convert.ex "./re5/main2.png" -strip -resize 652x -background white -gravity center -extent 652x "./re5/main2.jpg"
+
 convert.ex "./rerev/main1.png" -strip -resize 652x -background white -gravity center -extent 652x "./rerev/main1.jpg"
 convert.ex "./rerev/main2.png" -strip -resize 652x -background white -gravity center -extent 652x "./rerev/main2.jpg"
